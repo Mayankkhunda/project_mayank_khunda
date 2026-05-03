4ac3030cf710bda055de4a7bec35036837fb6466
Mayankkhunda <mayankkhunda@gmail.com>
2026-05-03 10:59:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,27 +1,20 @@
-# =========================
 # TRAINING PARAMETERS
-# =========================
+
 batchsize = 32
 epochs = 10
 learning_rate = 0.001
 
-
-# =========================
 # IMAGE PARAMETERS
-# =========================
+
 resize_x = 128
 resize_y = 128
 input_channels = 3
 
-
-# =========================
 # DATASET PARAMETERS
-# =========================
+
 num_classes = 17
 
-
-# =========================
 # PATHS
-# =========================
+
 data_path = "data"
 model_save_path = "checkpoints/final_weights.pth"
